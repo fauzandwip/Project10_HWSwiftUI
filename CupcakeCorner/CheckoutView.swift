dfe48471450a9beb28d09163cf5532816dd71dec
fauzandp <fauzan27dp@gmail.com>
2023-06-26 15:45:03 +0700

    Project10_HWSwiftUI | CupcakeCorner App create CheckoutView UI add cost variable to count cost fetch img with AsyncImage Preparing for checkout

--- CupcakeCorner/CheckoutView.swift
+++ CupcakeCorner/CheckoutView.swift
@@ -11,12 +11,44 @@ struct CheckoutView: View {
     @ObservedObject var order: Order
     
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ScrollView {
+            VStack {
+                AsyncImage(url: URL(string: "https://hws.dev/img/cupcakes@3x.jpg")) { image in
+                    image
+                        .resizable()
+                        .scaledToFit()
+                        .clipShape(RoundedRectangle(cornerRadius: 30))
+                } placeholder: {
+                    ProgressView()
+                }
+                .frame(height: 233)
+                .padding(.horizontal, 20)
+                
+                HStack {
+                    Text("Your total is")
+                        .font(.title)
+                    Text("\(order.cost, format: .currency(code: "USD"))")
+                        .font(.title)
+                        .foregroundColor(.mint)
+                }
+                
+                Button("Place Order", action: {})
+                    .font(.headline.bold())
+                    .foregroundColor(.white)
+                    .padding()
+                    .background(.mint)
+                    .clipShape(Capsule())
+            }
+        }
+        .navigationTitle("Check Out")
+        .navigationBarTitleDisplayMode(.inline)
     }
 }
 
 struct CheckoutView_Previews: PreviewProvider {
     static var previews: some View {
-        CheckoutView(order: Order())
+        NavigationView {
+            CheckoutView(order: Order())
+        }
     }
 }
